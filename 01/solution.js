f27f8b982eb9fb2432a6570dffa5061225e19309
Adam Markon <amarkon@hubspot.com>
2021-12-01 10:43:07 -0700

    part 2

--- 01/solution.js
+++ 01/solution.js
@@ -3,9 +3,33 @@ const path = require('path');
 
 const distances = fs.readFileSync(path.resolve('input.txt')).toString('utf-8').split('\n').map(val => parseInt(val, 10)).filter(Boolean);
 
-const increasingDistances = distances.reduce((increasing, currDistance, currIdx) => {
-  if (currDistance > (distances[currIdx - 1] || Infinity)) return increasing + 1;
-  return increasing;
-}, 0);
+function countIncreasingDistances() {
+  const increasingDistances = distances.reduce((increasing, currDistance, currIdx) => {
+    if (currDistance > (distances[currIdx - 1] || Infinity)) return increasing + 1;
+    return increasing;
+  }, 0);
 
-console.log(`Total count of increasing distances: ${increasingDistances}`);
+  console.log(`Total count of increasing distances: ${increasingDistances}`);
+}
+
+function sum(ints) {
+  return ints.reduce((prev, val) => prev + val, 0);
+}
+
+function countIncreasingWindows(size = 3) {
+  const increasingWindows = distances.reduce((increasing, __, currIdx) => {
+    // need to make sure we can calculate the full window
+    if (currIdx < (size - 1)) return increasing;
+    const cWindowRaw = (distances.slice(currIdx - (size - 1), currIdx + 1));
+    const pWindowRaw = (distances.slice(currIdx - size, currIdx));
+    const cWindow = sum(cWindowRaw);
+    const pWindow = sum(pWindowRaw);
+    if (cWindow > pWindow && pWindow !== 0) return increasing + 1;
+    return increasing;
+  }, 0)
+
+  console.log(`Total count of increasing windows: ${increasingWindows}`);
+}
+
+countIncreasingDistances();
+countIncreasingWindows();
